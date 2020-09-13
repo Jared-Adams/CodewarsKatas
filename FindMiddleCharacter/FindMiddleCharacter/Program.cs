4f4a391a6b317f2c2c13f83a88d286d8957d8e5f
Jared-Adams <jared.LearnProgramming@gmail.com>
2020-09-12 22:05:06 -0400

    code golf

---
+++ FindMiddleCharacter/FindMiddleCharacter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FindMiddleCharacter
+{
+    class Program
+    {
+        /// <summary>
+        /// You are going to be given a word. Your job is to return the middle character of the word. 
+        /// If the word's length is odd, return the middle character.
+        /// If the word's length is even, return the middle 2 characters.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMiddle("Middle"));
+            Console.ReadLine();
+        }
+
+        public static string GetMiddle(string s) => s.Length % 2 == 0 ? $"{s[(s.Length / 2) - 1]}{s[s.Length / 2]}" : $"{s[s.Length / 2]}";
+    }
+}
---
+++ SumOfPositive/SumOfPositive/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+namespace SumOfPositive
+{
+    class Program
+    {
+        /// <summary>
+        /// Simply get the sum of all positive numbers in a given array
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PositiveSum(new int[] { -1, 2, 3, 4, -5 })); //9
+            Console.ReadLine();
+        }
+
+        public static int PositiveSum(int[] arr) => arr.Where(x => x > 0).Sum();
+    }
+}
